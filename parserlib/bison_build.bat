e42f0d9e8075c7993cd586051751522729adf975
bgonzax <bgonzales20001@gmail.com>
2015-04-04 12:30:30 -0700

    init bison lr parser.

---
+++ parserlib/bison_build.bat
@@ -0,0 +1,1 @@
+bison -d -v calc.y -o calc.c -p calc_
---
+++ parserlib/stdafx.h
@@ -0,0 +1,20 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <tchar.h>
+#include <string>
+#include <cmath>
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+// TODO: reference additional headers your program requires here
