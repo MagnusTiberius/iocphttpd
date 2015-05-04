24ffdb16223a98d81482d33ce3de6375f659f9a8
bgonzax <bgonzales20001@gmail.com>
2015-05-04 16:54:04 -0700

    ssl

---
+++ iocphttpl/OpenSsl.cpp
@@ -0,0 +1,25 @@
+#include "OpenSsl.h"
+
+
+OpenSsl::OpenSsl()
+{
+}
+
+
+OpenSsl::~OpenSsl()
+{
+}
+
+
+void OpenSsl::Init()
+{
+	SSL_load_error_strings();
+	OpenSSL_add_ssl_algorithms();
+
+	ctx = SSL_CTX_new(SSLv23_server_method());
+
+	cctx = SSL_CONF_CTX_new();
+	SSL_CONF_CTX_set_flags(cctx, SSL_CONF_FLAG_SERVER);
+	SSL_CONF_CTX_set_flags(cctx, SSL_CONF_FLAG_CERTIFICATE);
+	SSL_CONF_CTX_set_ssl_ctx(cctx, ctx);
+}
---
+++ iocphttpl/OpenSsl.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "stdafx.h"
+
+class OpenSsl
+{
+public:
+	OpenSsl();
+	~OpenSsl();
+
+	void Init();
+
+private:
+	char *port = "*:4433";
+	BIO *ssl_bio, *tmp;
+	SSL_CTX *ctx;
+	SSL_CONF_CTX *cctx;
+	char buf[512];
+	BIO *in = NULL;
+	int ret = 1, i;
+};
+
+/*
+
+http://p-nand-q.com/programming/windows/building_openssl_with_visual_studio_2013.html
+
+http://developer.covenanteyes.com/building-openssl-for-visual-studio/
+
+*/
