a91180d162a3f433119f97efc45421067639aedb
bgonzales20001 <bgonzales20001@gmail.com>
2016-03-07 19:58:38 -0800

    Add demo website

---
+++ websitecarconfig/IocpHttpd.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "stdafx.h"
+#include "SocketCompletionPortServer.h"
+
+using namespace IOCPHTTPL;
+
+class IocpHttpd : public SocketCompletionPortServer
+{
+public:
+	IocpHttpd();
+	~IocpHttpd();
+
+	void EvalGet(HttpRequest *httpRequest, HttpResponse *httpResponse);
+	void EvalPost(HttpRequest *httpRequest, HttpResponse *httpResponse);
+
+	static void HandleHome(HttpRequest *httpRequest, HttpResponse *httpResponse);
+	static void HandleTest(HttpRequest *httpRequest, HttpResponse *httpResponse);
+	static void HandleInit(HttpRequest *httpRequest, HttpResponse *httpResponse);
+	static void HandleJsonTest(HttpRequest *httpRequest, HttpResponse *httpResponse);
+	static void HandleJsonTestTwo(HttpRequest *httpRequest, HttpResponse *httpResponse);
+	static void HandleJsonUrlParam1(HttpRequest *httpRequest, HttpResponse *httpResponse);
+
+private:
+
+};
+
---
+++ websitecarconfig/stdafx.h
@@ -0,0 +1,21 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+#include <string>
+
+#include "json.h"
+
+#define BUFSIZMAX		8*1024
+#define BUFSIZMIN		1024
+#define DATA_BUFSIZE	8192
+
+
+// TODO: reference additional headers your program requires here
