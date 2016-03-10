7dc15cc3f5a5c1b85ebc35b37d12f2fec0f20ab4
bgonzales20001 <bgonzales20001@gmail.com>
2016-03-09 16:48:00 -0800

    HostServer JS

---
+++ wwwmelonJS-2.0.x/examples/platformer/js/hostserver.js
@@ -0,0 +1,23 @@
+
+
+(function () {
+
+	me.hostserver = (function() {
+
+		var tick;
+
+		function TimerTick() {
+
+		}
+
+		function Start() {
+			tick = setInterval(TimerTick, 1000);
+		}
+
+		function Stop() {
+			clearInterval(tick);
+		}
+
+	});
+
+})();
