07f1f6331b236e92dc412dae415bab8560139a05
bgonzales20001 <bgonzales20001@gmail.com>
2016-03-12 17:37:03 -0800

    update website

---
+++ www/static/js/autobilecontroller.js
@@ -0,0 +1,16 @@
+app.controller("myCtrl", function($scope) {
+    $scope.firstName	= "John";
+    $scope.lastName= "Doe";
+    $scope.myWelcome2 = "akjslf jsdkfj sdlkfjsdklfjsdklfsjfklsdjf klsdj fklsdjflkjsdfklsdjklf";
+});
+
+
+//app.controller('myCtrl2', function($scope, $http) {
+//	debugger
+//	var url = location.protocol+'//'+location.hostname+(location.port ? ':'+location.port: '')+"/json/testtwo";
+  //  $http.get(url)
+  //  .then(function(response) {
+  //  	debugger
+  //      $scope.myWelcome = response.data;
+//    });
+//});
